266a36180b24e44e97bcd3200d0425d32fdaa679
Gustavo <gustavoo.sec@gmail.com>
2025-02-03 19:07:32 -0300

    text interpolation

--- src/components/AnotherComponent.vue
+++
@@ -1,3 +0,0 @@
-<script setup></script>
-<template>Nasted Component</template>
-<style scoped></style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,16 @@
+<script setup>
+const myMessage = 'Yellow World'
+const myNumber = 100
+
+function add(x, y) {
+  return x + y
+}
+</script>
+
+<template>
+  <h1>{{ myMessage }}</h1>
+  <h1>{{ myNumber }}</h1>
+  <h1>{{ add(10, 10) }}</h1>
+</template>
+
+<style scoped></style>
