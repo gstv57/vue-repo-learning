0d28dc3198e7759750d47e6406108374b88c4bbe
Gustavo <gustavoo.sec@gmail.com>
2025-02-03 18:59:56 -0300

    components

---
+++ src/components/AnotherComponent.vue
@@ -0,0 +1,3 @@
+<script setup></script>
+<template>Nasted Component</template>
+<style scoped></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<script setup>
+import AnotherComponent from './AnotherComponent.vue'
+</script>
+
+<template>
+  <h1>Hello World</h1>
+  <AnotherComponent />
+</template>
+
+<style scoped></style>
