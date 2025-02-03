555d7e6164850d55f7c2324d3d392b81a0869c70
Gustavo <gustavoo.sec@gmail.com>
2025-02-03 19:18:07 -0300

    attribute bindings

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -1,16 +1,27 @@
 <script setup>
-const myMessage = 'Yellow World'
-const myNumber = 100
+// const myLink = 'https://www.google.com.br'
+// const myImage =
+//   'https://plus.unsplash.com/premium_photo-1683910767532-3a25b821f7ae?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D'
 
-function add(x, y) {
-  return x + y
+// const altTextImage = 'Only a random picture'
+
+// const imageWidth = 400
+// const imageHeight = 400
+
+// dinamic binding
+
+const imageInfo = {
+  src: 'https://plus.unsplash.com/premium_photo-1683910767532-3a25b821f7ae?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D',
+  alt: 'Only a random picture',
+  width: 400,
+  height: 400,
 }
 </script>
 
 <template>
-  <h1>{{ myMessage }}</h1>
-  <h1>{{ myNumber }}</h1>
-  <h1>{{ add(10, 10) }}</h1>
+  <!-- <a :href="myLink" target="_blank">Google</a> -->
+  <!-- <img :src="myImage" :alt="altTextImage" :width="imageWidth" :height="imageHeight" /> -->
+  <img :="imageInfo" />
 </template>
 
 <style scoped></style>
