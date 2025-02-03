0c6a050d27244580919ab49ffce2d84f16dcfedc
Gustavo <gustavoo.sec@gmail.com>
2025-02-03 20:05:18 -0300

    event handle pt 1

---
+++ src/components/MyEvent.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+let count = ref(0)
+
+const increment = () => count.value++
+const decrement = () => count.value--
+</script>
+
+<template>
+  <h1>Count: {{ count }}</h1>
+  <button @:click="increment">+</button>
+  <button @:click="decrement">-</button>
+</template>
