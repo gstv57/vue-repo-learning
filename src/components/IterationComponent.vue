90f66dbf79d0e8b7db7a19a5c18ed185f69846a5
Gustavo de Moraes <gustavoo.sec@gmail.com>
2025-02-07 18:00:50 -0300

    iteration with matriz

--- src/components/IterationComponent.vue
+++ src/components/IterationComponent.vue
@@ -1,50 +1,21 @@
 <script setup>
 import { ref } from 'vue'
 let books = ref([
-  {
-    id: 1,
-    title: 'Harry Potter',
-    author: 'J.K. Rowling',
-    genre: 'Fantasy',
-    publicationYear: 1997,
-  },
-  {
-    id: 2,
-    title: 'The Lord of the Rings',
-    author: 'J.R.R. Tolkien',
-    genre: 'Fantasy',
-    publicationYear: 1954,
-  },
-  {
-    id: 3,
-    title: 'A Song of Ice and Fire',
-    author: 'George R.R. Martin',
-    genre: 'Fantasy',
-    publicationYear: 1996,
-  },
-  {
-    id: 4,
-    title: 'The Chronicles of Narnia',
-    author: 'C.S. Lewis',
-    genre: 'Fantasy',
-    publicationYear: 1950,
-  },
-  {
-    id: 5,
-    title: "The Hitchhiker's Guide to the Galaxy",
-    author: 'Douglas Adams',
-    genre: 'Science Fiction',
-    publicationYear: 1979,
-  },
+  ['The Great Gatsby', 'F. Scott Fitzgerald'],
+  ['The Catcher in the Rye', 'J.D. Salinger'],
+  ['To Kill a Mockingbird'],
+  ['1984', 'George Orwell', 'Dystopian'],
+  ['Pride and Prejudice', 'Jane Austen'],
+  ['Wuthering Heights', 'Emily Bronte'],
+  ['Jane Eyre', 'Charlotte Bronte'],
+  ['Moby-D'],
 ])
 </script>
 
 <template>
-  <ul v-for="{ id, title, author, genre, publicationYear } in books" :key="id">
-    <li>ID: {{ id }}</li>
-    <li>Title: {{ title }}</li>
-    <li>Author: {{ author }}</li>
-    <li>Genre: {{ genre }}</li>
-    <li>Publication Year: {{ publicationYear }}</li>
+  <ul v-for="(book, i) in books" :key="i">
+    <li v-for="(b, index) in book" :key="index">
+      {{ b }}
+    </li>
   </ul>
 </template>
