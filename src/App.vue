00ff6fe85312ae2dd1816e57581d97b608310a46
Gustavo de Moraes <gustavoo.sec@gmail.com>
2025-02-07 20:59:53 -0300

    custom validator

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,7 @@
 <script setup>
-import { ref } from 'vue'
-import DynamicPropsComponent from './components/DynamicPropsComponent.vue'
-import StaticPropsComponent from './components/StaticPropsComponent.vue'
-import ChaningPropsValue from './components/ChaningPropsValue.vue'
-import PropValidation from './components/PropValidation.vue'
-import ComplexProps from './components/ComplexProps.vue'
-const firstName = ref('Gustavo')
-const lastName = ref('de Moraes')
-
-const myFavoriteNumber = ref(7)
+import CustomValidator from './components/CustomValidator.vue'
 </script>
 
 <template>
-  <StaticPropsComponent name="Gustavo" />
-  <hr />
-  <DynamicPropsComponent :firstName="firstName" :lastName="lastName" />
-  <hr />
-  <ChaningPropsValue :myFavoriteNumber="myFavoriteNumber" />
-  <hr />
-  <PropValidation :age="24" />
-  <hr />
-  <ComplexProps
-    :friends="['Noveli', 'João do Bife', 'Lelis', 'Duran', 'Marcola', 'Malik']"
-    :userInfo="{ name: 'Gustavo', age: 24, location: ['Sorocaba', 'SP'] }"
-  />
+  <CustomValidator name="Gustavo" :age="20" password="impossiblePassword" />
 </template>
