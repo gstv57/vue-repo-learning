1ecd916ad9c7b623a8cf8fa8c66525921564cff7
Gustavo de Moraes <gustavoo.sec@gmail.com>
2025-02-07 20:07:08 -0300

    dynamic props

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script setup>
+import { ref } from 'vue'
+import DynamicPropsComponent from './components/DynamicPropsComponent.vue'
 import StaticPropsComponent from './components/StaticPropsComponent.vue'
+
+const firstName = ref('Gustavo')
+const lastName = ref('de Moraes')
 </script>
 
 <template>
   <StaticPropsComponent name="Gustavo" />
+  <hr />
+  <DynamicPropsComponent :firstName="firstName" :lastName="lastName" />
+  <hr />
 </template>
