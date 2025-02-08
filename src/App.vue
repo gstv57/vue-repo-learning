a29b5c210d1086c146eace585cc7314719483000
Gustavo de Moraes <gustavoo.sec@gmail.com>
2025-02-08 10:11:05 -0300

    fallback content

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
 import SlotComponent from './components/SlotComponent.vue'
+import FallbackContent from './components/FallbackContent.vue'
 </script>
 
 <template>
-  <SlotComponent> <h1>Content</h1> </SlotComponent>
+  <SlotComponent>
+    <h1>Content</h1>
+    <h2>Content</h2>
+  </SlotComponent>
+  <hr />
+  <FallbackContent> </FallbackContent>
 </template>
---
+++ src/components/FallbackContent.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <slot>
+    <h1>This is a fall back / Default Content</h1>
+  </slot>
+</template>
