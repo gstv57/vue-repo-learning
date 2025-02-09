5968258713fc9f995659e85c162fb0b55488533b
Gustavo de Moraes <gustavoo.sec@gmail.com>
2025-02-09 08:00:20 -0300

    watch pt2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
 import BasicComponent from './components/BasicComponent.vue'
+import ReactiveObject from './components/ReactiveObject.vue'
 </script>
 
 <template>
   <BasicComponent />
+  <hr />
+  <ReactiveObject />
 </template>
---
+++ src/components/ReactiveObject.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { reactive, watch } from 'vue'
+const state = reactive({ username: 'gstv' })
+
+watch(
+  () => state.username,
+  (newValue, oldValue) => {
+    console.log('old value: ', oldValue)
+    console.log('new value: ', newValue)
+  },
+)
+</script>
+<template>
+  <h1>{{ state.username }}</h1>
+  <button @click="state.username = 'Gstv'">Change Name</button>
+</template>
