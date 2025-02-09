3bebb2f7debebbf6c2024bebaf750114978981e2
Gustavo de Moraes <gustavoo.sec@gmail.com>
2025-02-09 08:29:27 -0300

    template ref pt2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
-import BasicRef from './components/BasicRef.vue'
+import FunctionRef from './components/FunctionRef.vue'
 </script>
 
 <template>
-  <BasicRef />
+  <FunctionRef />
 </template>
---
+++ src/components/FunctionRef.vue
@@ -0,0 +1,12 @@
+<script setup>
+const myRefFunc = (el) => {
+  console.log(el.textContent)
+  el.textContent = 'Text changed'
+  el.style.color = 'crimson'
+}
+</script>
+<template>
+  <div>
+    <h1 :ref="myRefFunc">Hello</h1>
+  </div>
+</template>
