3b7084737dc2e879a6ed3e6592bc194a235dc86f
Gustavo de Moraes <gustavoo.sec@gmail.com>
2025-02-09 08:05:48 -0300

    watch pt3

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import BasicComponent from './components/BasicComponent.vue'
 import ReactiveObject from './components/ReactiveObject.vue'
+import MultipleSources from './components/MultipleSources.vue'
 </script>
 
 <template>
   <BasicComponent />
   <hr />
   <ReactiveObject />
+  <hr />
+  <MultipleSources />
 </template>
---
+++ src/components/MultipleSources.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref, watch } from 'vue'
+const username = ref('gstv')
+const counter = ref(0)
+
+watch([username, counter], (newValue, oldValue) => {
+  console.log('Old Value: ', oldValue)
+  console.log('New Value: ', newValue)
+})
+</script>
+<template>
+  <h1>{{ username }}</h1>
+  <h1>{{ counter }}</h1>
+
+  <button @click="username = 'gstv57'">Change Name</button>
+  <button @click="counter++">Increment Counter</button>
+</template>
